e6b3e40c43e791f70dd58cff555a0f4dc561fad8
yj <developer@yilu.co>
2018-11-19 17:03:03 +0800

    yilu to yilu-tech

--- src/ComplexSearch.php
+++ src/ComplexSearch.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Yilu\ComplexSearch;
+namespace YiluTech\ComplexSearch;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
--- src/FunctionFactory.php
+++ src/FunctionFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Yilu\ComplexSearch;
+namespace YiluTech\ComplexSearch;
 
 class FunctionFactory
 {
